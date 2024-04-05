39e8e282806babb2ed36f86e769c08ca83a21f91
felipe-gds <felipe.jc@icloud.com>
2024-04-04 22:26:49 -0300

    Finalizacao do curso

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
-# Testes automatizados com Cypress - Básico
+# Curso-Basico-Cypress
 
-👋 Seja bem-vindo(a)!
+Um projeto básico elaborado no curso do **Walmyr Filho, embaixador Cypress.io.**
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai adorar este curso. ❤️
+## Pré-Requisitos
 
-## O que você vai aprender
+Foi necessária a instalação do Node.js e o npm.
 
-Durante o curso de testes automatizados com Cypress (básico), você vai aprender:
+> Usei no meu projeto a versão`20.11.1` e `10.2.4` do Node.js e npm, respectivamente. No curso é usado uma versão anterior.
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar _upload_ de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como executar os testes em um _pipeline_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
+## Instalação
 
-## Vamos começar?
+Run `npm install` (ou `npm i` para versão menor) 
+para instalar as dependências de desenvolvimento.
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+## Testes
 
-___
+Você pode rodar os testes simulando um desktop ou uma tela de dispositivo móvel.
+### Desktop
 
-Este é mais um curso da [**Escola Talking About Testing**](https://udemy.com/user/walmyr).
+Execute `npm test` (ou `npm t` para a versão curta) para executar o teste no modo headless.
+
+Ou execute `npm run cy:open` para abrir o Cypress no modo interativo.
+### Mobile
+
+Execute `npm run test:mobile` para executar o teste no modo headless.
+
+Ou execute `npm run cy:open:mobile` para abrir o Cypress no modo interativo.
+
+
+## Apoie este projeto
+
+Se você quiser apoiar este projeto, deixe um ⭐.
--- cypress/e2e/tat.cy.js
+++ cypress/e2e/tat.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+    const TRES_SEGUNDOS = 3000
     beforeEach(function() {
         cy.visit('./src/index.html')
 })
@@ -20,12 +21,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.clock() // congela relógio navegador
+
         cy.get('#firstName').type('Felipe')
         cy.get('#lastName').type('Teste')
         cy.get('#email').type('felipeteste.com')
         cy.get('#open-text-area').type('Texto de teste')
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
+        
+        cy.tick(TRES_SEGUNDOS) //acelera o relógio
+
+        cy.get('.error').should('not.be.visible')
     })
 
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
@@ -43,8 +50,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.clock()
+       
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
+
+        cy.tick(TRES_SEGUNDOS)
+        cy.get('.error').should('not.be.visible')
+
     })
 
     it('envia o formuário com sucesso usando um comando customizado', function() {
@@ -57,12 +70,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .select('YouTube')
             .should('have.value', 'youtube')
     })    
-
+// testanto com uso da biblioteca lodash onde repete 5 vezes o teste 
+    Cypress._.times(5, function () {
     it('seleciona um produto (Mentoria) por seu valor (value)', function() {
         cy.get('#product')
             .select('mentoria')
             .should('have.value', 'mentoria')
     })
+})
 
     it('seleciona um produto (Blog) por seu índice', function() {
         cy.get('#product')    
@@ -93,9 +108,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })  
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.clock()
+
         cy.get('#phone-checkbox').check()
         cy.fillMandatoryFieldsAndSubmit()
         cy.get('.error').should('be.visible')
+
+        cy.tick(TRES_SEGUNDOS)
+        cy.get('.error').should('not.be.visible')
     })
      
     it('seleciona um arquivo da pasta fixtures', function() {
@@ -137,6 +157,46 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('testa a página da política de privacidade de forma independente', function() {
         cy.visit('/src/privacy.html')
         cy.get('#title').should('contain', 'CAC TAT - Política de privacidade')
+    })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+    })
+
+    it('preenche a area de texto usando o comando invoke', function() {
+        const textoGrande = Cypress._.repeat('Texto grande ', 20)
+        
+        cy.get('#open-text-area')
+            .invoke('val', textoGrande) //ctrl V
+            .should('have.value', textoGrande)
+    })
+
+    it('faz uma requisição HTTP', function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response)  {
+            expect(response.status).to.equal(200)
+            expect(response.statusText).to.eq('OK')
+            expect(response.body).contains('CAC TAT')
+        })
+    })
+
+    it.only('desafio encontre o gato', function() {
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
     })
 })
 
